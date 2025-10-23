96c558def09be0df01d5701cd22d666f3ea852c4
hay-oon <hayooncode@gmail.com>
2025-10-23 22:27:04 +0900

    feat: 자동차 전진 로직 구현

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,13 +1,14 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
 public class RacingGame {
     private List<Car> cars = new ArrayList<>();
-    
-    public void run(){
+
+    public void run() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputName = Console.readLine();
 
@@ -19,5 +20,14 @@ public class RacingGame {
         System.out.println("시도할 횟수는 몇 회인가요?");
         String inputTry = Console.readLine();
         int tryCount = Integer.parseInt(inputTry);
+
+        for (int i = 1; i <= tryCount; i++){
+            for (Car car : cars) {
+                int randomNumber = Randoms.pickNumberInRange(0, 9);
+                if (randomNumber > 3) {
+                    car.move();
+                }
+            }
+        }
     }
 }
