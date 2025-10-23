20f7c7293b1ba45007699b9bc593c7b5f6931ff8
hay-oon <hayooncode@gmail.com>
2025-10-23 22:49:19 +0900

    feat: 최대 전진 거리 및 우승자 판정 로직 추가

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 public class RacingGame {
     private List<Car> cars = new ArrayList<>();
+    private List<String> winners = new ArrayList<>();
+    private int maxPosition = 0;
 
     public void run() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -21,6 +23,7 @@ public class RacingGame {
         String inputTry = Console.readLine();
         int tryCount = Integer.parseInt(inputTry);
 
+        // 시도 횟수만큼 경주 실행
         for (int i = 1; i <= tryCount; i++){
             for (Car car : cars) {
                 int randomNumber = Randoms.pickNumberInRange(0, 9);
@@ -29,5 +32,21 @@ public class RacingGame {
                 }
             }
         }
+
+        // 최대 전진 거리 판정
+        for (Car car : cars) {
+            if (car.getPosition() > maxPosition) {
+                maxPosition = car.getPosition();
+            }
+        }
+
+        // 우승자 판정
+        for (Car car : cars) {
+            if (car.getPosition() == maxPosition) {
+                winners.add(car.getName());
+            }
+        }
+
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
     }
 }
