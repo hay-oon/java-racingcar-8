ba764bc69ee6628688e6252647f9fa409cb6a7e6
hay-oon <hayooncode@gmail.com>
2025-10-23 21:42:18 +0900

    feat: 자동차 이름 입력 시 Car 객체를 리스트에 추가하도록 수정

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,15 +1,20 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RacingGame {
+    private List<Car> cars = new ArrayList<>();
+    
     public void run(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
 
-        String [] arrayNames = input.split(",");
+        String[] arrayNames = input.split(",");
         for (String name : arrayNames) {
-            new Car(name);
+            cars.add(new Car(name.trim()));
         }
+        
     }
 }
