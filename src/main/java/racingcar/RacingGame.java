620466fda97456cbef36b531fc626efe92605d93
hay-oon <hayooncode@gmail.com>
2025-10-23 22:00:51 +0900

    feat: 시도 횟수 입력 기능 추가

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -9,12 +9,15 @@ public class RacingGame {
     
     public void run(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String input = Console.readLine();
+        String inputName = Console.readLine();
 
-        String[] arrayNames = input.split(",");
+        String[] arrayNames = inputName.split(",");
         for (String name : arrayNames) {
             cars.add(new Car(name.trim()));
         }
-        
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String inputTry = Console.readLine();
+        int tryCount = Integer.parseInt(inputTry);
     }
 }
